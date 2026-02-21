34eba9da865f150fb8bf1885c028cf6da102fa7f
wyvernbw <iamwyvern511@gmail.com>
2026-02-21 21:49:59 +0200

    utils: add key macro

--- mana-tui-utils/src/lib.rs
+++ mana-tui-utils/src/lib.rs
@@ -13,3 +13,43 @@ impl Ecs for &mut World {}
 impl Ecs for &World {}
 
 impl EcsMut for &mut World {}
+
+// TODO: lock behind crossterm feature
+#[macro_export]
+macro_rules! key {
+    // Variant with no arguments, e.g. Enter, Esc
+    ($code:ident, $kind:ident) => {
+        ratatui::crossterm::event::KeyEvent {
+            code: ratatui::crossterm::event::KeyCode::$code,
+            kind: ratatui::crossterm::event::KeyEventKind::$kind,
+            modifiers: ratatui::crossterm::event::KeyModifiers::NONE,
+            ..
+        }
+    };
+    // Variant with arguments, e.g. Char('x'), Char(_)
+    ($code:ident ( $($arg:tt)* ), $kind:ident) => {
+        ratatui::crossterm::event::KeyEvent {
+            code: ratatui::crossterm::event::KeyCode::$code($($arg)*),
+            kind: ratatui::crossterm::event::KeyEventKind::$kind,
+            modifiers: ratatui::crossterm::event::KeyModifiers::NONE,
+            ..
+        }
+    };
+    // Variant with no arguments and optional modifiers
+    ($code:ident, $kind:ident, $mods:expr ) => {
+        ratatui::crossterm::event::KeyEvent {
+            code: ratatui::crossterm::event::KeyCode::$code,
+            kind: ratatui::crossterm::event::KeyEventKind::$kind,
+            modifiers: $mods,
+            ..
+        }
+    };
+    ($code:ident ( $($arg:tt)* ), $kind:ident, $mods:pat) => {
+        ratatui::crossterm::event::KeyEvent {
+            code: ratatui::crossterm::event::KeyCode::$code($($arg)*),
+            kind: ratatui::crossterm::event::KeyEventKind::$kind,
+            modifiers: $mods,
+            ..
+        }
+    };
+}
